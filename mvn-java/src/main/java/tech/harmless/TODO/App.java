4727b7243eb3d0c325409a3a90ede0206cf551f0
harmless-tech <git@harmless.tech>
2025-09-17 16:57:50 -0400

    Add template for Maven and Java

---
+++ mvn-java/src/main/java/tech/harmless/TODO/App.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2025 harmless-tech
+ * SPDX-License-Identifier: TODO
+ *
+ * TODO
+ */
+package tech.harmless.TODO;
+
+import org.tinylog.Logger;
+
+public final class App {
+
+    public static void main(String[] args) {
+        System.setProperty("logger.dir", "./tmp/TODO/logs");
+        Logger.info("App started!");
+    }
+}
